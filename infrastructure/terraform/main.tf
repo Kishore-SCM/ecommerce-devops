a917479f6acb01fc12afa3f096b86ea7634c282d
Kishore-SCM <kishoredevops681993@gmail.com>
2026-04-15 10:39:21 +0530

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -37,13 +37,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
   cluster_name    = "ecommerce-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.33"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
   cluster_endpoint_public_access = true
   eks_managed_node_groups = {
     general = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       min_size = 2; max_size = 6; desired_size = 3
       labels = { role = "general" }
     }
